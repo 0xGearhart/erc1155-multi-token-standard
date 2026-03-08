15beebdb335a3fd3e9fb25212915ebb112a0431e
0xGearhart <geardev26@gmail.com>
2026-03-08 14:55:47 -0700

    complete stage 3 chunk 1

--- test/invariant/Handeler.t.sol
+++ test/invariant/Handeler.t.sol
@@ -2,9 +2,95 @@
 
 pragma solidity 0.8.33;
 
-import {DeployGameItems} from "../../script/DeployGameItems.s.sol";
-import {HelperConfig} from "../../script/HelperConfig.s.sol";
 import {GameItems} from "../../src/GameItems.sol";
-import {Test, console2} from "forge-std/Test.sol";
+import {IAccessControl} from "@openzeppelin/contracts/access/IAccessControl.sol";
+import {Test} from "forge-std/Test.sol";
 
-contract Handler is Test {}
+contract Handler is Test {
+    GameItems public immutable gameItems;
+    address public immutable minter;
+    address public immutable uriSetter;
+    address public immutable burner;
+    address public immutable unauthorized;
+
+    uint256 public constant MAX_TRACKED_ID = 8;
+
+    mapping(uint256 id => uint256 amount) public mintedById;
+    mapping(uint256 id => uint256 amount) public burnedById;
+
+    constructor(GameItems gameItems_, address minter_, address uriSetter_, address burner_, address unauthorized_) {
+        gameItems = gameItems_;
+        minter = minter_;
+        uriSetter = uriSetter_;
+        burner = burner_;
+        unauthorized = unauthorized_;
+    }
+
+    function mintAsMinter(uint256 id, uint256 amount) external {
+        id = bound(id, 1, MAX_TRACKED_ID);
+        amount = bound(amount, 1, type(uint96).max);
+
+        vm.prank(minter);
+        gameItems.mint(burner, id, amount, "");
+        mintedById[id] += amount;
+    }
+
+    function mintAsUnauthorized(uint256 id, uint256 amount) external {
+        id = bound(id, 1, MAX_TRACKED_ID);
+        amount = bound(amount, 1, type(uint96).max);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(IAccessControl.AccessControlUnauthorizedAccount.selector, unauthorized, gameItems.MINTER_ROLE())
+        );
+        vm.prank(unauthorized);
+        gameItems.mint(unauthorized, id, amount, "");
+    }
+
+    function burnAsBurner(uint256 id, uint256 amount) external {
+        id = bound(id, 1, MAX_TRACKED_ID);
+
+        uint256 balance = gameItems.balanceOf(burner, id);
+        if (balance == 0) {
+            vm.prank(minter);
+            gameItems.mint(burner, id, 1, "");
+            mintedById[id] += 1;
+            balance = 1;
+        }
+
+        amount = bound(amount, 1, balance);
+
+        vm.prank(burner);
+        gameItems.burn(id, amount);
+        burnedById[id] += amount;
+    }
+
+    function burnAsUnauthorized(uint256 id, uint256 amount) external {
+        id = bound(id, 1, MAX_TRACKED_ID);
+        amount = bound(amount, 1, type(uint96).max);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IAccessControl.AccessControlUnauthorizedAccount.selector, unauthorized, gameItems.BURNER_ROLE()
+            )
+        );
+        vm.prank(unauthorized);
+        gameItems.burn(id, amount);
+    }
+
+    function setUriAsSetter(uint256 salt) external {
+        string memory newUri = string.concat("ipfs://invariant/", vm.toString(salt), "/{id}.json");
+        vm.prank(uriSetter);
+        gameItems.setURI(newUri);
+    }
+
+    function setUriAsUnauthorized(uint256 salt) external {
+        string memory newUri = string.concat("ipfs://unauthorized/", vm.toString(salt), "/{id}.json");
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IAccessControl.AccessControlUnauthorizedAccount.selector, unauthorized, gameItems.URI_SETTER_ROLE()
+            )
+        );
+        vm.prank(unauthorized);
+        gameItems.setURI(newUri);
+    }
+}
--- test/invariant/Invariants.t.sol
+++ test/invariant/Invariants.t.sol
@@ -2,11 +2,45 @@
 
 pragma solidity 0.8.33;
 
-import {DeployGameItems} from "../../script/DeployGameItems.s.sol";
-import {HelperConfig} from "../../script/HelperConfig.s.sol";
 import {GameItems} from "../../src/GameItems.sol";
 import {Handler} from "./Handeler.t.sol";
 import {StdInvariant} from "forge-std/StdInvariant.sol";
-import {Test, console2} from "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 
-contract Invariants is StdInvariant, Test {}
+contract Invariants is StdInvariant, Test {
+    GameItems internal gameItems;
+    Handler internal handler;
+
+    address internal defaultAdmin = makeAddr("defaultAdmin");
+    address internal minter = makeAddr("minter");
+    address internal uriSetter = makeAddr("uriSetter");
+    address internal burner = makeAddr("burner");
+    address internal unauthorized = makeAddr("unauthorized");
+
+    uint48 internal constant INITIAL_DELAY = 1;
+    string internal constant INITIAL_URI = "ipfs://mock/{id}.json";
+
+    function setUp() external {
+        gameItems = new GameItems(defaultAdmin, minter, uriSetter, burner, INITIAL_DELAY, INITIAL_URI);
+        handler = new Handler(gameItems, minter, uriSetter, burner, unauthorized);
+        targetContract(address(handler));
+    }
+
+    function invariant_SupplyConservationForTrackedIds() external view {
+        for (uint256 id = 1; id <= handler.MAX_TRACKED_ID(); id++) {
+            uint256 expectedSupply = handler.mintedById(id) - handler.burnedById(id);
+            assertEq(gameItems.totalSupply(id), expectedSupply, "tracked supply mismatch");
+        }
+    }
+
+    function invariant_RoleBoundariesRemainIntact() external view {
+        assertTrue(gameItems.hasRole(gameItems.DEFAULT_ADMIN_ROLE(), defaultAdmin));
+        assertTrue(gameItems.hasRole(gameItems.MINTER_ROLE(), minter));
+        assertTrue(gameItems.hasRole(gameItems.URI_SETTER_ROLE(), uriSetter));
+        assertTrue(gameItems.hasRole(gameItems.BURNER_ROLE(), burner));
+
+        assertFalse(gameItems.hasRole(gameItems.MINTER_ROLE(), unauthorized));
+        assertFalse(gameItems.hasRole(gameItems.URI_SETTER_ROLE(), unauthorized));
+        assertFalse(gameItems.hasRole(gameItems.BURNER_ROLE(), unauthorized));
+    }
+}
